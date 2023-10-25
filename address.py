e38bbde19ac0ad2c757c5dd91de8a794e6af98ac
JalaaJ <abusalehjalaa1@gmail.com>
2023-10-25 13:31:15 +0300

    added the Address class

---
+++ address.py
@@ -0,0 +1,9 @@
+class Address:
+    def __init__(self, country, state, city, street, postal):
+        self.country = country
+        self.state = state
+        self.city = city
+        self.streetAddress = street
+        self.postalCode = postal
+
+    
